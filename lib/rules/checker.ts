b2753815ab3c22b44e269ae474250b3e0a12fc11
Kory Marks <kory.marks@gc.com>
2022-03-23 16:31:01 -0400

    changes from review

--- lib/rules/checker.ts
+++ lib/rules/checker.ts
@@ -1,32 +1,38 @@
 
 export enum RuleTypeEnum {
-    STATE_CHECKER = 'state_checker',
+    VALUE_CHECKER = 'value_checker',
     RANGE_CHECKER = 'range_checker',
     LIST_CHECKER = 'list_checker',
+    REGEX_MATCHER = 'regex_matcher',
 }
 
-type RuleTypeForState = {
-    type: RuleTypeEnum.STATE_CHECKER;
+type RuleTypeForValueChecker = {
+    type: RuleTypeEnum.VALUE_CHECKER;
     value: string | boolean | number;
 }
 
-type RuleTypeForList = {
+type RuleTypeForListChecker = {
     type: RuleTypeEnum.LIST_CHECKER;
     value: (string | boolean | number)[];
 }
 
-type RuleTypeForRange = {
+type RuleTypeForRangeChecker = {
     type: RuleTypeEnum.RANGE_CHECKER;
     value: { min: number; max: number }[];
+}
+
+type RuleTypeForRegexMatcher = {
+    type: RuleTypeEnum.REGEX_MATCHER;
+    value: string;
 }
 
 export type RuleDefinition = {
     id: string;
     description: string;
-    rule: RuleTypeForList | RuleTypeForState | RuleTypeForRange;
+    rule: RuleTypeForListChecker | RuleTypeForValueChecker | RuleTypeForRangeChecker | RuleTypeForRegexMatcher;
     state: {
         is_enabled: boolean;
-        is_expired: null | boolean;
+        is_expired?: boolean;
     };
 }
 
@@ -40,30 +46,36 @@ export type RuleCheck = {
     }
 }
 
-function isStateChecker(rule: RuleDefinition['rule']): rule is RuleTypeForState {
-    return rule.type === RuleTypeEnum.STATE_CHECKER;
+function isValueChecker(rule: RuleDefinition['rule']): rule is RuleTypeForValueChecker {
+    return rule.type === RuleTypeEnum.VALUE_CHECKER;
 }
 
-function isListChecker(rule: RuleDefinition['rule']): rule is RuleTypeForList {
+function isListChecker(rule: RuleDefinition['rule']): rule is RuleTypeForListChecker {
     return rule.type === RuleTypeEnum.LIST_CHECKER;
 }
 
-function isRangeChecker(rule: RuleDefinition['rule']): rule is RuleTypeForRange {
+function isRangeChecker(rule: RuleDefinition['rule']): rule is RuleTypeForRangeChecker {
     return rule.type === RuleTypeEnum.RANGE_CHECKER;
 }
 
+function isRegexMatcher(rule: RuleDefinition['rule']): rule is RuleTypeForRegexMatcher {
+    return rule.type === RuleTypeEnum.REGEX_MATCHER;
+}
+
 function getCheckedValue(ruleDefinition: RuleDefinition, value: boolean | string | number): boolean {
-    if (isStateChecker(ruleDefinition.rule)) {
+    if (isValueChecker(ruleDefinition.rule)) {
         return ruleDefinition.rule.value === value;
     }  
     if (isListChecker(ruleDefinition.rule)) {
         return ruleDefinition.rule.value.includes(value);
     }
     if (isRangeChecker(ruleDefinition.rule)) {
-        console.log('Yes this is a state checker', ruleDefinition.rule.value, value)
         return ruleDefinition.rule.value.every(({min, max}) => 
             value >= min && value <= max
         )
+    }
+    if (isRegexMatcher(ruleDefinition.rule)) {
+        return new RegExp(ruleDefinition.rule.value).test(value.toString())
     }
 
     throw new Error('Could not handle the rule type');
@@ -71,34 +83,15 @@ function getCheckedValue(ruleDefinition: RuleDefinition, value: boolean | string
 
 export const checker = (feature: string, value: boolean | string | number ): RuleCheck => {
     // TODO: use the feature which is the name in namespace:category:id format
-    // const ruleDefinition: RuleDefinition = {
-    //     id: "send_advertising_email",
-    //     description: "Checks whether we should send the advertising email or not",	  
-    //     rule: {
-    //         type: RuleTypeEnum.STATE_CHECKER,
-    //         value: false
-    //     },
-    //     state: {
-    //         is_enabled: true,
-    //         is_expired: null
-    //     }
-    // };
     const ruleDefinition: RuleDefinition = {
-        "id": "score_on_range",
-        "description": "Defines the scoring range to consider when using AI to determine a score play.",
-      
-        "rule": {
-          "type": RuleTypeEnum.RANGE_CHECKER,
-          "value": [
-            {
-              "min": 0.2,
-              "max": 1
-            }]
+        id: "vpn_ip_range",
+        description: "Defines the scoring range to consider when using AI to determine a score play.",
+        rule: {
+          type: RuleTypeEnum.REGEX_MATCHER,
+          value: "^100\\.22\\.33\\.([1-9]|[1-9]\\d|1\\d\\d|2[0-4]\\d|250)$"
         },
-      
-        "state": {
-          "is_enabled": true,
-          "is_expired": null
+        state: {
+          is_enabled: true
         }
       };
 
