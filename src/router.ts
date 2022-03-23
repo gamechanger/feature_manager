1b8aa41f63f0f9bf5e00015ad9c95438c1a5647a
Namaswi <namaswic@gmail.com>
2022-03-23 16:01:44 -0400

    Add feature file on post

--- src/router.ts
+++ src/router.ts
@@ -15,12 +15,26 @@ router
 		context.response.body = 'response';
 	})
 	.post('/feature/:namespace/:category/:id', (context) => {
+		console.log(context.params);
+
+		let fileName = [context.params.namespace, context.params.category, context.params.id].join(":") ;
+		let featurePath = './features/' + fileName + ".json";
+		const successMessage = addFeature(featurePath, context.request.body);
+		console.log(successMessage);
 		context.response.body = 'response';
 	})
 	.post('/check/:namespace/:category/:id', (context) => {
-
-
 		context.response.body = 'response';
 	});
 
 export default router;
+
+function addFeature(toPath: string, data: object): string {
+	try {
+	  Deno.writeTextFileSync(toPath, JSON.stringify(data));
+  
+	  return "Written to " + toPath;
+	} catch (e) {
+	  return e.message;
+	}
+  }
